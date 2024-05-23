5d45820ca05d8105f76532519a68174ab453a238
Bill Tyros <me@strategineer.com>
2024-05-22 22:23:55 -0400

    Implement showing multiple results, add more stuff to GM page, name tables properly

--- assets/js/site_rpg.js
+++ assets/js/site_rpg.js
@@ -8,7 +8,7 @@ function testRolls() {
     console.log(result.die); // '1d6'
 }
 
-function randomizeTable(title, header, rows) {
+function randomizeTable(title, header, rows, count) {
   const table_config = {
     key: title,
     title: title,
@@ -16,13 +16,30 @@ function randomizeTable(title, header, rows) {
       default: rows,
     }
   };
+  const isDefaultCount = count === 1;
+  count = getDiceResult(count).value;
   const tableRoller = new TableRoller();
   const table = new RandomTable(table_config);
-  const result = tableRoller.getTableResult(table);
-  const headerColumns = header.split("|").map(c => c.trim()).filter(c => c)
-  const resultColumns = result[0].result.split("|").map(c => c.trim()).filter(c => c)
-  var resultRows = headerColumns.map(function(e, i) {
-    return `${e}: ${resultColumns[i]}`;
-  });
-  return resultRows.join("<br>");
+  var s = [];
+  var i = 0;
+  while (i < count) {
+    const result = tableRoller.getTableResult(table);
+    const headerColumns = header.split("|").map(c => c.trim()).filter(c => c)
+    const resultColumns = result[0].result.split("|").map(c => c.trim()).filter(c => c)
+    var resultRows = headerColumns.map(function(e, i) {
+      if (headerColumns.length == 1) {
+        return resultColumns[i];
+      }
+      return `${e}: ${resultColumns[i]}`;
+    });
+    s.push(resultRows.join("<br>"));
+    i += 1;
+  }
+  if (!isDefaultCount) {
+    const numberedResults = s.map(function(e, i) {
+      return `${i}: ${s[i]}`;
+    });
+    return `${count} ${title}:<br>${numberedResults.join("<br>")}`;
+  }
+  return s.join("<br>");
 }
